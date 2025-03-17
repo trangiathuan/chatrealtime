068115bd0ec90a0912f654d49cb7b76204d91c2c
trangiathuan <nakrothfaker@krothfakergmail.fakergmailcom>
2025-03-18 00:06:16 +0700

    .

--- frontend/src/configs/configAPI.js
+++ frontend/src/configs/configAPI.js
@@ -1,5 +1,7 @@
 
 
-const ENDPOIN_API = 'http://localhost:8888/api/v1/'
+//const ENDPOIN_API = 'http://localhost:8888/api/v1/'
+
+const ENDPOIN_API = 'https://message-bygiathuan.onrender.com/'
 
 export default ENDPOIN_API;
