333182a4eee3eff3044559052bbc83657218e75b
trangiathuan <nakrothfaker@krothfakergmail.fakergmailcom>
2025-03-18 00:24:26 +0700

    .

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -80,7 +80,7 @@ const Navbar = () => {
                     <a href="/" className="block hover:bg-blue-700 px-3 py-2 rounded">
                         Home
                     </a>
-                    <a href="/message/67d7dfce5d544218bc32382a" className="block hover:bg-blue-700 px-3 py-2 rounded">
+                    <a href="/message/67d7dfce5d544218bc32382a " className="block hover:bg-blue-700 px-3 py-2 rounded">
                         Massage
                     </a>
                     <a href="#" className="block hover:bg-blue-700 px-3 py-2 rounded">
