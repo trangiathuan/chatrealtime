b729e5d7f9e812e5308514aff9ad7a0547f470bf
trangiathuan <nakrothfaker@krothfakergmail.fakergmailcom>
2025-03-17 23:58:25 +0700

    .

--- backend/src/configs/databaseConfig.js
+++ backend/src/configs/databaseConfig.js
@@ -3,14 +3,22 @@ const mongoose = require("mongoose");
 
 exports.connectDatabase = async () => {
     try {
+        // const options = {
+        //     user: process.env.DB_USER,
+        //     pass: process.env.DB_PASSWORD
+        // };
+
+        // const uri = `${process.env.DB_HOST}/${process.env.DB_NAME}?authSource=admin`;
+
+        // await mongoose.connect(uri, options);
+
         const options = {
-            user: process.env.DB_USER,
-            pass: process.env.DB_PASSWORD
-        };
+            user: 'giathuan',
+            pass: '08022003',
+            dbName: 'Message'
+        }
+        await mongoose.connect('mongodb+srv://trangiathuan08022003.9zwyq.mongodb.net//', options);
 
-        const uri = `${process.env.DB_HOST}/${process.env.DB_NAME}?authSource=admin`;
-
-        await mongoose.connect(uri, options);
         return {
             EC: 0,
             Message: "Kết nối database thành công"
